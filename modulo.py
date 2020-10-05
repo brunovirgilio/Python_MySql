88aec8a69f96c48ae1f910f0a8c28137c3efde68
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-10-05 20:06:10 -0300

    parte3-v1

--- modulo.py
+++ modulo.py
@@ -31,20 +31,7 @@ class Tela:
 
     
 
-'''
-conexao = pymysql.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '',
-    database = 'db1'
-)
 
-cursor = conexao.cursor()
-cursor.execute('SHOW TABLES')
-
-for x in cursor:
-    print(x)
-'''
 
 
 
