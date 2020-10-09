1f77cd152868911050782bfa4538e616d1b91983
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-10-09 19:50:40 -0300

    parte4-v4-OK

--- modulo.py
+++ modulo.py
@@ -12,32 +12,34 @@ class Tela:
             [sg.Text('Qnt de moradores', size=(16,0)),sg.Input(key='linha3')],
             [sg.Text('Possui veículo?', size=(16,0))],
             [sg.Checkbox('Sim',key='sim'),sg.Checkbox('Não',key='nao')],
-            [sg.Button('Enviar')]            
+            [sg.Button('Incluir'),sg.Button('Excluir'),sg.Button('Exibir Lista'),sg.Button('Limpar Tela')],
+            [sg.Output(size=(60,10))]            
         ]
         #janela
-        janela = sg.Window('Dados Morador').layout(layout)
-        #extração
-        self.Button, self.values = janela.Read()
-
+        self.janela = sg.Window('Dados Morador').layout(layout)
+        
     def iniciar(self):
-        n = self.values['linha1']
-        a = self.values['linha2']
-        q = self.values['linha3']
-        vs = self.values['sim']
-        vn = self.values['nao']
-        
-        cursor = conexao.cursor()
-        incluir = 'INSERT INTO morador(nome, apt, qnt, vsim,vnao)VALUES(%s, %s, %s, %s, %s)'
-        valor =(str(n),str(a),str(q),bool(vs),bool(vn))
+        while True:
+            #extração
+            self.Button, self.values = self.janela.Read()
+            n = self.values['linha1']
+            a = self.values['linha2']
+            q = self.values['linha3']
+            vs = self.values['sim']
+            vn = self.values['nao']
+            
+            cursor = conexao.cursor()
+            incluir = 'INSERT INTO morador(nome, apt, qnt, vsim,vnao)VALUES(%s, %s, %s, %s, %s)'
+            valor =(str(n),str(a),str(q),bool(vs),bool(vn))
 
-        cursor.execute(incluir,valor)
-        conexao.commit()
-        
-        print(f'nome: {n}')
-        print(f'apt: {a}')
-        print(f'qnt: {q}')
-        print(f'veiculo sim: {vs}')
-        print(f'veiculo nao: {vn}')
+            cursor.execute(incluir,valor)
+            conexao.commit()
+            print('CARREGAMENTO CONCLUÍDO')
+            print(f'nome: {n}')
+            print(f'apt: {a}')
+            print(f'qnt: {q}')
+            print(f'veiculo sim: {vs}')
+            print(f'veiculo nao: {vn}')
 
     
 
