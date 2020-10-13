e26c9021a84d01f512d5c6f3936c797bf57717d2
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-10-13 19:59:57 -0300

    parte5-v1

--- modulo.py
+++ modulo.py
@@ -22,24 +22,37 @@ class Tela:
         while True:
             #extração
             self.Button, self.values = self.janela.Read()
-            n = self.values['linha1']
-            a = self.values['linha2']
-            q = self.values['linha3']
-            vs = self.values['sim']
-            vn = self.values['nao']
-            
-            cursor = conexao.cursor()
-            incluir = 'INSERT INTO morador(nome, apt, qnt, vsim,vnao)VALUES(%s, %s, %s, %s, %s)'
-            valor =(str(n),str(a),str(q),bool(vs),bool(vn))
+            if self.Button == 'Incluir':
+                n = self.values['linha1']
+                a = self.values['linha2']
+                q = self.values['linha3']
+                vs = self.values['sim']
+                vn = self.values['nao']
+                
+                cursor = conexao.cursor()
+                incluir = 'INSERT INTO morador(nome, apt, qnt, vsim,vnao)VALUES(%s, %s, %s, %s, %s)'
+                valor =(str(n),str(a),str(q),bool(vs),bool(vn))
 
-            cursor.execute(incluir,valor)
-            conexao.commit()
-            print('CARREGAMENTO CONCLUÍDO')
-            print(f'nome: {n}')
-            print(f'apt: {a}')
-            print(f'qnt: {q}')
-            print(f'veiculo sim: {vs}')
-            print(f'veiculo nao: {vn}')
+                cursor.execute(incluir,valor)
+                conexao.commit()
+                print('CARREGAMENTO CONCLUÍDO')
+                print(f'nome: {n}')
+                print(f'apt: {a}')
+                print(f'qnt: {q}')
+                print(f'veiculo sim: {vs}')
+                print(f'veiculo nao: {vn}')
+            
+            elif self.Button == 'Limpar Tela':
+                self.values['linha1'].update(value=' ')
+                
+
+               
+
+              
+                
+                
+
+            
 
     
 
