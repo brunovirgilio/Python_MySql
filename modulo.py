9c195d81eaa45df02d1b6078ef2bae17ae9d4ef4
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-10-27 21:02:37 -0300

    parte5-v6

--- modulo.py
+++ modulo.py
@@ -3,51 +3,52 @@ import PySimpleGUI as sg
 from database import *
 
 
-class Tela:
-    def __init__(self):
-        #layout
-        layout = [
-            [sg.Text('Nome Completo', size=(16,0)),sg.Input(key='linha1')],
-            [sg.Text('Apartamento', size=(16,0)),sg.Input(key='linha2')],
-            [sg.Text('Qnt de moradores', size=(16,0)),sg.Input(key='linha3')],
-            [sg.Text('Possui veículo?', size=(16,0))],
-            [sg.Checkbox('Sim',key='sim'),sg.Checkbox('Não',key='nao')],
-            [sg.Button('Incluir',key='incluir'),sg.Button('Excluir',key='excluir'),sg.Button('Exibir Lista',key='exibir'),sg.Button('Limpar Tela',key='limpar')],
-            [sg.Output(size=(60,10))]            
-        ]
-        #janela
-        self.janela = sg.Window('Dados Morador').layout(layout)
-        
-        
-    def iniciar(self):
-        self.Button, self.values = self.janela.Read()
-        
-        if self.Button == 'incluir':
-            n = self.values['linha1']
-            a = self.values['linha2']
-            q = self.values['linha3']
-            vs = self.values['sim']
-            vn = self.values['nao']
-            
-            cursor = conexao.cursor()
-            incluir = 'INSERT INTO morador(nome, apt, qnt, vsim,vnao)VALUES(%s, %s, %s, %s, %s)'
-            valor =(str(n),str(a),str(q),bool(vs),bool(vn))
 
-            cursor.execute(incluir,valor)
-            conexao.commit()
-            print('CARREGAMENTO CONCLUÍDO')
-            print(f'nome: {n}')
-            print(f'apt: {a}')
-            print(f'qnt: {q}')
-            print(f'veiculo sim: {vs}')
-            print(f'veiculo nao: {vn}')
-    
-        if self.Button == 'limpar':
-            self.values['linha1'].clear()
-            self.values['linha2'].clear()
-            self.values['linha3'].clear()
-            self.values['sim'].clear()
-            self.values['nao'].clear()
+layout = [
+    [sg.Text('Nome Completo', size=(16,0)),sg.Input(key='linha1')],
+    [sg.Text('Apartamento', size=(16,0)),sg.Input(key='linha2')],
+    [sg.Text('Qnt de moradores', size=(16,0)),sg.Input(key='linha3')],
+    [sg.Text('Possui veículo?', size=(16,0))],
+    [sg.Checkbox('Sim',key='sim'),sg.Checkbox('Não',key='nao')],
+    [sg.Button('Incluir',key='incluir'),sg.Button('Excluir',key='excluir'),sg.Button('Exibir Lista',key='exibir'),sg.Button('Limpar Tela',key='limpar')],
+    [sg.Output(size=(60,10))]
+    ]
+#janela
+janela = sg.Window('Dados Morador').layout(layout)
+        
+while True:      
+    event, values = janela.Read()
+    if event == sg.WIN_CLOSED:
+        break
+        
+    if event == 'incluir':
+        n = values['linha1']
+        a = values['linha2']
+        q = values['linha3']
+        vs = values['sim']
+        vn = values['nao']
+        
+        cursor = conexao.cursor()
+        incluir = 'INSERT INTO morador(nome, apt, qnt, vsim,vnao)VALUES(%s, %s, %s, %s, %s)'
+        valor =(str(n),str(a),str(q),bool(vs),bool(vn))
+
+        cursor.execute(incluir,valor)
+        conexao.commit()
+        print('CARREGAMENTO CONCLUÍDO')
+        print(f'nome: {n}')
+        print(f'apt: {a}')
+        print(f'qnt: {q}')
+        print(f'veiculo sim: {vs}')
+        print(f'veiculo nao: {vn}')
+
+    if event == 'limpar':
+        window['linha1'].clear()
+        window['linha2'].clear()
+        window['linha3'].clear()
+
+window.close()
+            
+            
 
                  
                 
