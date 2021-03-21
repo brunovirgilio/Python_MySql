4d87c499153f3ea0e17fb2d6a1057bae79d75544
Bruno Virgilio de Lima <bvdelima@gmail.com>
2021-03-21 12:21:23 -0300

    commit

--- database.py
+++ database.py
@@ -6,7 +6,3 @@ conexao = pymysql.connect(
     password = '',
     database ='db2'    
 )
-'''
-cursor = conexao.cursor()
-cursor.execute('CREATE TABLE morador(Id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), apt INT(3), qnt INT(2), vsim BOOLEAN, vnao BOOLEAN)')
-'''
